4749a52b1bdf60b08d686441003b21445ce65058
Natalia Jastrzębska <nataliak@10g.pl>
2015-02-16 13:24:53 +0100

    + Created Service class

---
+++ app/src/main/java/com/example/natalia/wifiguard/MyService.java
@@ -0,0 +1,31 @@
+package com.example.natalia.wifiguard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        
+    }
+}
